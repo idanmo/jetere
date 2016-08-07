96bf1ab2965b29fdeeb37e77dfcaec02b26c3b76
Idan Moyal <idan@gigaspaces.com>
2016-08-07 16:59:50 +0300

    minor fix

--- jetere/settings.py
+++ jetere/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'systests.apps.ReportsConfig',
+    'reports.apps.ReportsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
